b654d32b3adb561669c88a7613623d1ff5a10e07
Anton Belousov <enemysik@gmail.com>
2020-03-05 08:10:17 +0700

    add AST notation usage

--- interpreter.ts
+++ interpreter.ts
@@ -12,7 +12,27 @@ export class Token {
     return `Token(${this.type}, ${this.value})`;
   }
 }
-
+export abstract class AST { }
+export class BinOp extends AST {
+  public left: AST;
+  public op: Token;
+  public right: AST
+  constructor(left: AST, op: Token, right: AST) {
+    super()
+    this.left = left;
+    this.op = op;
+    this.right = right;
+  }
+}
+export class Num extends AST {
+  public token: Token;
+  public value: ValueType;
+  constructor(token: Token) {
+    super();
+    this.token = token;
+    this.value = token.value;
+  }
+}
 export class Lexer {
   text: string;
   pos: number;
@@ -87,8 +107,7 @@ export class Lexer {
     return new Token('EOF', null);
   }
 }
-
-export class Interpreter {
+export class Parser {
   public lexer: Lexer
   public currentToken: Token;
   constructor(lexer: Lexer) {
@@ -104,52 +123,83 @@ export class Interpreter {
     else
       this.error();
   }
-  factor() {
+  factor(): AST {
     const token = this.currentToken;
     if (token.type === 'Integer') {
       this.eat('Integer');
-      return token.value;
+      return new Num(token);
     } else if (token.type === 'LParen') {
       this.eat('LParen');
-      const result = this.expr();
+      const node = this.expr();
       this.eat('RParen');
-      return result;
+      return node;
     }
   }
-  term() {
-    let result = this.factor() as number;
+  term(): AST {
+    let node = this.factor();
     while ((['Div', 'Mul'] as TokenType[]).indexOf(this.currentToken.type) !== -1) {
       const token = this.currentToken;
       if (token.type === 'Mul') {
         this.eat('Mul');
-        result *= this.term() as number;
       } else if (token.type === 'Div') {
         this.eat('Div');
-        result /= this.term() as number;
       }
+      node = new BinOp(node, token, this.factor());
     }
-    return result;
+    return node;
   }
-  expr() {
-    let result = this.term() as number;
+  expr(): AST {
+    let node = this.term();
     while ((['Minus', 'Plus'] as TokenType[]).indexOf(this.currentToken.type) !== -1) {
       const token = this.currentToken;
       if (token.type === 'Minus') {
         this.eat('Minus');
-        result -= this.term() as number;
       } else if (token.type === 'Plus') {
         this.eat('Plus');
-        result += this.term() as number;
       }
+      node = new BinOp(node, token, this.term());
     }
-    return result;
+    return node;
+  }
+  parse() {
+    return this.expr();
+  }
+}
+export class Interpreter {
+  public parser: Parser;
+  constructor(parser: Parser) {
+    this.parser = parser;
+  }
+  visitBinOp(node: BinOp) {
+    if (node.op.type === 'Plus')
+      return this.visit(node.left) + this.visit(node.right);
+    if (node.op.type === 'Minus')
+      return this.visit(node.left) - this.visit(node.right);
+    if (node.op.type === 'Mul')
+      return this.visit(node.left) * this.visit(node.right);
+    if (node.op.type === 'Div')
+      return this.visit(node.left) / this.visit(node.right);
+  }
+  visitNum(node: Num) {
+    return node.value;
+  }
+  visit(node: AST) {
+    if (node instanceof BinOp)
+      return this.visitBinOp(node)
+    if (node instanceof Num)
+      return this.visitNum(node)
+  }
+  interpret() {
+    const tree = this.parser.parse();
+    return this.visit(tree);
   }
 }
 function main() {
   const text = '5 + 7 * (2 - 3)';
   const lexer = new Lexer(text);
-  const interpreter = new Interpreter(lexer);
-  const result = interpreter.expr();
+  const parser = new Parser(lexer);
+  const interpreter = new Interpreter(parser);
+  const result = interpreter.interpret();
   console.log(result);
 }
 main();
